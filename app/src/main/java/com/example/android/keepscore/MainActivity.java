eda604517274ea000d6ec736cc9d62de3efba02c
smoisset <smoisset@worldremit.com>
2018-02-03 18:23:23 +0000

    Implement onClick for Button

--- app/src/main/java/com/example/android/keepscore/MainActivity.java
+++ app/src/main/java/com/example/android/keepscore/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.android.keepscore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int mScore1 = 0;
+    int mScore2 = 0;
+    TextView mScoreText1;
+    TextView mScoreText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+    }
+
+    public void decreaseScore(View view) {
+
+        int viewID = view.getId();
+
+        switch (viewID) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    public void increaseScore(View view) {
+
+        int viewID = view.getId();
+
+        switch (viewID) {
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
     }
 }
