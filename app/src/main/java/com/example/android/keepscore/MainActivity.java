60b535f20b897b590f212dc409f1df2cd9b96334
smoisset <smoisset@worldremit.com>
2018-02-05 11:20:33 +0000

    Change theme from menu

--- app/src/main/java/com/example/android/keepscore/MainActivity.java
+++ app/src/main/java/com/example/android/keepscore/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android.keepscore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,6 +14,24 @@ public class MainActivity extends AppCompatActivity {
     int mScore2 = 0;
     TextView mScoreText1;
     TextView mScoreText2;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.night_mode) {
+
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+
+            recreate();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else {
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+
+        return true;
     }
 
     public void decreaseScore(View view) {
